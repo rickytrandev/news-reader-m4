60e5d727495b378e21d85a8392a707e841c719a1
Ricky Tran <therichardtran@gmail.com>
2024-03-07 14:36:50 -0700

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,266 @@
+{
+  "status": "ok",
+  "totalResults": 14610,
+  "articles": [
+      {
+          "source": {
+              "id": "wired",
+              "name": "Wired"
+          },
+          "author": "Andrew Couts",
+          "title": "How to Not Get Scammed Out of $50,000",
+          "description": "Plus: State-backed hackers test out generative AI, the US takes down a major Russian military botnet, and 100 hospitals in Romania go offline amid a major ransomware attack.",
+          "url": "https://www.wired.com/story/how-to-not-get-scammed-out-of-50000/",
+          "urlToImage": "https://media.wired.com/photos/65cfc0a11dbea08b10a4278c/191:100/w_1280,c_limit/How-To-Not-Get-Scammed-For-50k-Security-200392681-001.jpg",
+          "publishedAt": "2024-02-17T14:00:00Z",
+          "content": "While the response to Cowles tale has been a mix of praise and mockery, experts in online threats say its foolish to think youre too savvy to never fall for a professional scammer. The reality is, cr… [+3560 chars]"
+      },
+      {
+          "source": {
+              "id": null,
+              "name": "Gizmodo.com"
+          },
+          "author": "Artem Golub and Thomas Germain",
+          "title": "Why Bitcoin Won’t Reach $1,000,000",
+          "description": "Coindesk’s Andy Baehr says Bitcoin is just a normal, bread and butter investment now.",
+          "url": "https://gizmodo.com/why-bitcoin-won-t-reach-1-000-000-1851252185",
+          "urlToImage": "https://i.kinja-img.com/image/upload/c_fill,h_675,pg_1,q_80,w_1200/d27f57afb7ac3501d4d69ce6afff190b.jpg",
+          "publishedAt": "2024-02-13T18:15:00Z",
+          "content": "Coindesks Andy Baehr says Bitcoin is just a normal, bread and butter investment now."
+      },
+      {
+          "source": {
+              "id": "ars-technica",
+              "name": "Ars Technica"
+          },
+          "author": "Ashley Belanger",
+          "title": "Bitcoin price hits record $69K after SEC approvals fueled $7B in investments",
+          "description": "SEC chair warns bitcoin is still \"volatile\" and linked to \"illicit activity.\"",
+          "url": "https://arstechnica.com/tech-policy/2024/03/bitcoin-price-hits-record-69k-after-sec-approvals-fueled-7b-in-investments/",
+          "urlToImage": "https://cdn.arstechnica.net/wp-content/uploads/2024/03/GettyImages-1872368024-760x380.jpg",
+          "publishedAt": "2024-03-05T19:07:13Z",
+          "content": "20\r\nBitcoin's price hit a record high Tuesday, surging above $69,000 and notching past its prior peak price of $68,991.85 recorded in 2021.\r\nAfter bitcoin's price sunk in 2022 amid a cryptocurrency i… [+4343 chars]"
+      },
+      {
+          "source": {
+              "id": "business-insider",
+              "name": "Business Insider"
+          },
+          "author": "prosen@insider.com (Phil Rosen)",
+          "title": "El Salvador's millennial crypto-loving president says the country's bitcoin holdings are up more than 40%",
+          "description": "Nayib Bukele said if El Salvador sold all of the bitcoin in its coffers today, it would bank roughly $41.6 million in profit.",
+          "url": "https://markets.businessinsider.com/news/currencies/bitcoin-price-el-salvador-millennial-president-bukele-investment-cryptocurrency-finance-2024-2",
+          "urlToImage": "https://i.insider.com/65df846290413ab8e1d796e4?width=1200&format=jpeg",
+          "publishedAt": "2024-02-29T12:46:09Z",
+          "content": "President Nayib Bukele said El Salvador won't sell its bitcoin investment.\r\nEmerson Flores/APHOTOGRAFIA/Getty Images\r\nEl Salvador is up big on its bitcoin bet, according to its millennial president N… [+2026 chars]"
+      },
+      {
+          "source": {
+              "id": "business-insider",
+              "name": "Business Insider"
+          },
+          "author": "prosen@insider.com (Phil Rosen)",
+          "title": "Why crypto is surging back to all-time highs and shrugging off high interest rates",
+          "description": "Bitcoin suffered during the Fed's tightening cycle, but is rallying once again as a supply-demand imbalance offsets rate pressures.",
+          "url": "https://markets.businessinsider.com/news/currencies/bitcoin-price-outlook-explained-cryptocurrency-record-high-interest-rates-fed-2024-3",
+          "urlToImage": "https://i.insider.com/65e603d790413ab8e1d8e646?width=1200&format=jpeg",
+          "publishedAt": "2024-03-04T18:47:06Z",
+          "content": "Bitcoin is in the midst of a prolonged rebound back to levels last seen when interest rates were near zero and pixelated artwork was regularly selling for millions. \r\nOn Monday, bitcoin spiked more t… [+3350 chars]"
+      },
+      {
+          "source": {
+              "id": null,
+              "name": "NPR"
+          },
+          "author": "David Gura",
+          "title": "Bitcoin hits a record high. Here are 4 things to know about this spectacular rally",
+          "description": "Bitcoin hit a record high, marking a remarkable comeback from a period known as the \"crypto winter.\" A big catalyst behind the gains has been the approval of new bitcoin investment funds.",
+          "url": "https://www.npr.org/2024/03/05/1235865112/bitcoin-record-high-rally-crypto-sec-etf",
+          "urlToImage": "https://media.npr.org/assets/img/2024/03/04/gettyimages-19217562081_wide-eba7c909ba5e0cb64e10c0f882f70414cf0a5847-s1400-c100.jpg",
+          "publishedAt": "2024-03-05T18:38:02Z",
+          "content": "Bitcoin hit a record high following a rally sparked by the Securities and Exchange Commission's approval of bitcoin exchange-traded funds.\r\nDan Kitwood/Getty Images\r\nBitcoin is trading at a new all-t… [+5142 chars]"
+      },
+      {
+          "source": {
+              "id": null,
+              "name": "Slashdot.org"
+          },
+          "author": "BeauHD",
+          "title": "Reddit Discloses Bitcoin and Ether Investments In IPO Filing",
+          "description": "As part of its IPO filing with the SEC, Reddit disclosed that it has invested some of its excess cash in bitcoin, ether and Polygon. From a report: Based on the document, the firm now holds BTC and ETH in its balance sheet. Notably, Reddit filing came as part…",
+          "url": "https://slashdot.org/story/24/02/28/2143227/reddit-discloses-bitcoin-and-ether-investments-in-ipo-filing",
+          "urlToImage": "https://a.fsdn.com/sd/topics/bitcoin_64.png",
+          "publishedAt": "2024-02-28T23:20:00Z",
+          "content": "Based on the document, the firm now holds BTC and ETH in its balance sheet. Notably, Reddit filing came as part of the IPO registration statement with the SEC. Apart from ETH and BTC, the filing reve… [+1185 chars]"
+      },
+      {
+          "source": {
+              "id": "business-insider",
+              "name": "Business Insider"
+          },
+          "author": "Lloyd Lee",
+          "title": "Merrill and Wells Fargo are taking Bitcoin ETFs even more mainstream",
+          "description": "Bank of America's Merill and Wells Fargo are offering some of their clients bitcoin exchange-traded funds, which have recently seen a surge in demand.",
+          "url": "https://www.businessinsider.com/merill-wells-fargo-bitcoin-etf-mainstream-products-wealth-management-2024-2",
+          "urlToImage": "https://i.insider.com/65e10465ce7f1785b2e51ea7?width=1200&format=jpeg",
+          "publishedAt": "2024-02-29T23:04:54Z",
+          "content": "Bank of America's Merrill and Wells Fargo are offering spot bitcoin ETF's to some of their clients as the investment vehicle surges in demand, Bloomberg ReportedHannes P Albert/picture alliance via G… [+2414 chars]"
+      },
+      {
+          "source": {
+              "id": "business-insider",
+              "name": "Business Insider"
+          },
+          "author": "Emily Stewart",
+          "title": "Bitcoin will keep going boom and bust until we all die",
+          "description": "Investors are stuck in a perpetual boom-and-bust cycle where they put money into bitcoin every few years, only to get burned by a price crash.",
+          "url": "https://www.businessinsider.com/bitcoin-all-time-high-etfs-crypto-price-crash-boom-bust-2024-3",
+          "urlToImage": "https://i.insider.com/65e78a5790413ab8e1d97e2e?width=1200&format=jpeg",
+          "publishedAt": "2024-03-06T10:47:01Z",
+          "content": "Bitcoin investors are stuck in a perpetual boom-and-bust cycle where they get back on the bitcoin horse every few years only to get burned.Getty Images; Alyssa Powell/BI\r\nBitcoin's been back on the u… [+7318 chars]"
+      },
+      {
+          "source": {
+              "id": "business-insider",
+              "name": "Business Insider"
+          },
+          "author": "Lina Batarags",
+          "title": "Jack Dorsey showed up to the Super Bowl rocking a 'Satoshi' t-shirt",
+          "description": "Dorsey — who cofounded and was formerly the CEO of Twitter, now known as X — was photographed at the Super Bowl sitting with Beyonce and Jay-Z.",
+          "url": "https://www.businessinsider.com/jack-dorsey-satoshi-tshirt-nirvana-super-bowl-bitcoin-jayz-2024-2",
+          "urlToImage": "https://i.insider.com/65c9a3ad610ab33aa5deea9c?width=1200&format=jpeg",
+          "publishedAt": "2024-02-12T06:10:54Z",
+          "content": "Jack Dorsey, center, wrote a \"Satoshi\" t-shirt to the Super Bowl.Carmen Mandato / Staff / Getty Images\r\n<ul><li>Jack Dorsey was at the Super Bowl.</li><li>He wore a t-shirt that says \"Satoshi,\" embla… [+2446 chars]"
+      },
+      {
+          "source": {
+              "id": "business-insider",
+              "name": "Business Insider"
+          },
+          "author": "Theron Mohamed",
+          "title": "Gary Shilling says the S&P 500 may crash 30%, a recession will hit — and bitcoin and AI are both overhyped",
+          "description": "Stocks are poised to disappoint, recession alarms are flashing, and speculation is rife in tech and crypto, says market prophet Gary Shilling.",
+          "url": "https://markets.businessinsider.com/news/stocks/gary-shilling-stock-market-outlook-crash-spx-recession-bitcoin-ai-2024-2",
+          "urlToImage": "https://i.insider.com/65c49f4943bb77284ba4d201?width=1200&format=jpeg",
+          "publishedAt": "2024-02-08T09:36:13Z",
+          "content": "Gary Shilling.A. Gary Shilling & Co.\r\n<ul>\n<li>The S&P 500 may be headed for a 30% crash, a legendary market forecaster has warned.</li>\n<li>Gary Shilling said stocks are \"very expensive and very dis… [+3820 chars]"
+      },
+      {
+          "source": {
+              "id": "business-insider",
+              "name": "Business Insider"
+          },
+          "author": "Dan DeFrancesco",
+          "title": "What the latest inflation data means for interest rates",
+          "description": "In today's big story, we're looking at what a hotter-than-expected inflation report means for markets and the economy.",
+          "url": "https://www.businessinsider.com/inflation-data-impact-on-fed-interest-rate-cut-2024-2",
+          "urlToImage": "https://i.insider.com/65ccd21594aa8ee8e84b2f0a?width=1200&format=jpeg",
+          "publishedAt": "2024-02-14T14:47:16Z",
+          "content": "Getty Images\r\n<ul><li>This post originally appeared in the Insider Today newsletter.</li><li>You can sign up for Business Insider's daily newsletter here.</li></ul>Happy Valentine's Day! Don't worry … [+6433 chars]"
+      },
+      {
+          "source": {
+              "id": "business-insider",
+              "name": "Business Insider"
+          },
+          "author": "Alex Bitter",
+          "title": "Bots are some DoorDash and Walmart Spark delivery gig workers' dirty secret",
+          "description": "Gig workers on delivery apps like DoorDash are turning to bots to claim high-paying orders as pay falls and competition rises for drivers.",
+          "url": "https://www.businessinsider.com/gig-workers-using-bots-claim-orders-delivery-apps-doordash-spark-2024-1",
+          "urlToImage": "https://i.insider.com/655e5be522cf74a5739bbdff?width=1200&format=jpeg",
+          "publishedAt": "2024-02-10T10:42:02Z",
+          "content": "A DoorDash delivery worker in New York City.Ribeiro/New York Daily News/Tribune News Service via Getty Images\r\n<ul><li>\"Bots,\" or apps that make it easier to claim orders, are proliferating on delive… [+5555 chars]"
+      },
+      {
+          "source": {
+              "id": "business-insider",
+              "name": "Business Insider"
+          },
+          "author": "Theron Mohamed",
+          "title": "Market prophet Gary Shilling warns stocks could crash 30% — and says the odds are 'very much in favor of a recession'",
+          "description": "Star forecaster Gary Shilling rang the alarm on stocks and growth, dismissed de-dollarization fears, and predicted the housing market would recover.",
+          "url": "https://markets.businessinsider.com/news/stocks/stock-market-forecast-crash-shilling-recession-economy-dollar-housing-debt-2024-3",
+          "urlToImage": "https://i.insider.com/608948dc3f0560001881c769?width=1200&format=jpeg",
+          "publishedAt": "2024-03-05T12:18:19Z",
+          "content": "Gary Shilling.Bloomberg\r\n<ul>\n<li>Stocks may crash 30% and a recession could hit within months, Gary Shilling said.</li>\n<li>The star forecaster said stocks are super pricey and flagged several signs… [+4198 chars]"
+      },
+      {
+          "source": {
+              "id": "business-insider",
+              "name": "Business Insider"
+          },
+          "author": "Dan DeFrancesco",
+          "title": "Citi is betting big on high-profile executives to turn things around",
+          "description": "In today's big story, we're looking at the reshuffling of top executives at two of the biggest US banks.",
+          "url": "https://www.businessinsider.com/citi-new-executive-hires-viswas-raghavan-2024-2",
+          "urlToImage": "https://i.insider.com/65ce3b56a610a9069d5942c9?width=1200&format=jpeg",
+          "publishedAt": "2024-02-27T14:17:24Z",
+          "content": "SAUL LOEB/Getty Images\r\n<ul><li>This post originally appeared in the Insider Today newsletter.</li><li>You can sign up for Insider's daily newsletter here.</li></ul>Hello! There's donating to charity… [+6027 chars]"
+      },
+      {
+          "source": {
+              "id": "business-insider",
+              "name": "Business Insider"
+          },
+          "author": "Peter Kafka",
+          "title": "Why Jack Dorsey's failure led to Elon Musk's arrival at Twitter",
+          "description": "In \"Battle for the Bird,\" author Kurt Wagner shows how a checked-out Jack Dorsey left an opening for Elon Musk. I asked Wagner what else he learned.",
+          "url": "https://www.businessinsider.com/elon-musk-twitter-x-jack-dorsey-ceo-book-kurt-wagner-2024-2",
+          "urlToImage": "https://i.insider.com/65d4c1eca610a9069d5a7c58?width=1200&format=jpeg",
+          "publishedAt": "2024-02-20T17:13:57Z",
+          "content": "Elon Musk was able to swoop in to buy Twitter after Jack Dorsey's hands-off leadership style left an opening.Jim Watson/AFP via Getty Images\r\n<ul><li>Elon Musk's chaotic takeover of Twitter was enabl… [+10530 chars]"
+      },
+      {
+          "source": {
+              "id": null,
+              "name": "ReadWrite"
+          },
+          "author": "Sophie Atkinson",
+          "title": "Bitcoin price surges beyond $65,000 as records beckon",
+          "description": "Bitcoin has reached a two-year high today (Monday 4 March) as it jumped to above $65,000 in the early hours […]\nThe post Bitcoin price surges beyond $65,000 as records beckon appeared first on ReadWrite.",
+          "url": "https://readwrite.com/bitcoin-bounces-beyond-65000-as-records-beckon/",
+          "urlToImage": "https://readwrite.com/wp-content/uploads/2024/03/pierre-borthiry-peiobty-vBCVcWUyvyM-unsplash-scaled.jpg",
+          "publishedAt": "2024-03-04T11:42:29Z",
+          "content": "Bitcoin has reached a two-year high today (Monday 4 March) as it jumped to above $65,000 in the early hours in Europe.\r\nMonday (Mar. 4) has seen the price rise to $65,537 which is the crypto assets h… [+1548 chars]"
+      },
+      {
+          "source": {
+              "id": null,
+              "name": "ReadWrite"
+          },
+          "author": "Suswati Basu",
+          "title": "Bitcoin price: crypto market hits record high, first time since 2021",
+          "description": "For the first time since November 2021, the price of Bitcoin has surged past $60,000, setting new record highs in […]\nThe post Bitcoin price: crypto market hits record high, first time since 2021 appeared first on ReadWrite.",
+          "url": "https://readwrite.com/bitcoin-price-crypto-market-hits-record-high-first-time-since-2021/",
+          "urlToImage": "https://readwrite.com/wp-content/uploads/2024/03/Bitcoin-price-crypto-market-hits-record-high-first-time-since-2021-900x600.png",
+          "publishedAt": "2024-03-01T17:18:11Z",
+          "content": "For the first time since November 2021, the price of Bitcoin has surged past $60,000, setting new record highs in local currencies across numerous global markets.\r\nThe Bitcoin price rose to $63,968 o… [+2940 chars]"
+      },
+      {
+          "source": {
+              "id": null,
+              "name": "ReadWrite"
+          },
+          "author": "Sophie Atkinson",
+          "title": "Bitcoin and gold prices shatter previous highs",
+          "description": "The ultra-modern and traditional markets coincided on Mar 5, as both Bitcoin and gold saw record-breaking highs occur on the […]\nThe post Bitcoin and gold prices shatter previous highs appeared first on ReadWrite.",
+          "url": "https://readwrite.com/bitcoin-and-gold-prices-shatter-previous-highs/",
+          "urlToImage": "https://readwrite.com/wp-content/uploads/2024/03/p2fOhXKdQPqdhr0tbXdxTQ.png",
+          "publishedAt": "2024-03-06T16:23:20Z",
+          "content": "The ultra-modern and traditional markets coincided on Mar 5, as both Bitcoin and gold saw record-breaking highs occur on the same day.\r\nIn the early hours in Europe on Monday 4 March, Bitcoin jumped … [+1785 chars]"
+      },
+      {
+          "source": {
+              "id": null,
+              "name": "ReadWrite"
+          },
+          "author": "Sam Shedden",
+          "title": "Coinbase crashed as Bitcoin soared to record high",
+          "description": "Angry Coinbase users slammed the cryptocurrency trading app after it crashed again on Tuesday (Mar.5), the same day Bitcoin hit […]\nThe post Coinbase crashed as Bitcoin soared to record high appeared first on ReadWrite.",
+          "url": "https://readwrite.com/coinbase-crashed-as-bitcoin-soared-to-record-high/",
+          "urlToImage": "https://readwrite.com/wp-content/uploads/2024/03/NfPvWsHYT8e0fGymX9HfZQ.png",
+          "publishedAt": "2024-03-06T10:44:24Z",
+          "content": "Angry Coinbase users slammed the cryptocurrency trading app after it crashed again on Tuesday (Mar.5), the same day Bitcoin hit a new all-time high price of $69,000 after over two years of waiting.\r\n… [+2292 chars]"
+      }
+  ]
+}
