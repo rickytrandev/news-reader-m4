43c07c297ffae847ab27d7f0311fa9463363a7c6
Ricky Tran <therichardtran@gmail.com>
2024-03-09 17:02:41 -0700

    Feat: add API calls to fetch queries and data on load

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -2,15 +2,16 @@ import "./Landing.css";
 import { Header } from "../Header/Header";
 import { Article } from "../Article/Article";
 
-export function Landing({ data }) {
+export function Landing({ searchResults, data, error }) {
   return (
     <>
-      <h1 className="trending" >Trending</h1>
+      <h1 className="trending">{searchResults.length ? `Results` : `Trending`}</h1>
       <main>
+        {error && <h2>{error}</h2>}
         <section className="articles">
-        <Article data={data} />
+          <Article data={data} />
         </section>
       </main>
     </>
-  )
+  );
 }
